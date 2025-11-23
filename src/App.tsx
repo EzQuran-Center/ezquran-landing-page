1a8734a981cfc87edd0c3ca9c0ca150e5a58a4be
SyahrilSulaiman <syahrilrevolusi@gmail.com>
2025-11-23 09:04:44 +0800

    teng commit - apply scroll to top when route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
@@ -8,6 +8,7 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Registration from './components/Registration';
 import { AuthProvider } from './contexts/AuthContext';
+import { useEffect } from 'react';
 
 function HomePage() {
   return (
@@ -23,11 +24,23 @@ function HomePage() {
   );
 }
 
+function ScrollToTop() {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]); // Triggered whenever route changes
+
+	return null;
+}
+
 function App() {
   console.log("env : ", import.meta.env.MODE)
+
   return (
     <AuthProvider>
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<><Navbar /><Registration /><Footer /></>} />
