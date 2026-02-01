2faeacbf12e48780afb4ad1af002b7d29e6db705
SyahrilSulaiman <syahrilrevolusi@gmail.com>
2026-02-02 00:57:15 +0800

    teng commit - initialize facebook pixel

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { initFacebookPixel } from './lib/facebookPixel.ts';
+
+// Initialize Facebook Pixel
+initFacebookPixel();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
