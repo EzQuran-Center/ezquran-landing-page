6ceae9e53b355563704a3e375a67041b86e5cf30
SyahrilSulaiman <syahrilrevolusi@gmail.com>
2026-02-02 10:30:27 +0800

    teng commit - clean code

--- src/lib/facebookPixel.ts
+++ src/lib/facebookPixel.ts
@@ -1,26 +1,29 @@
-import ReactPixel from 'react-facebook-pixel';
+import ReactPixel from "react-facebook-pixel";
 
-const PIXEL_ID = '25985735157727349';
+const PIXEL_ID = "25985735157727349";
 
 export const initFacebookPixel = () => {
-    ReactPixel.init(PIXEL_ID);
-    ReactPixel.pageView();
+	ReactPixel.init(PIXEL_ID);
+	ReactPixel.pageView();
 };
 
 export const trackPageView = () => {
-    ReactPixel.pageView();
+	ReactPixel.pageView();
 };
 
-export const trackPurchase = (value: number, currency: string = 'MYR') => {
-    ReactPixel.track('Purchase', { value, currency });
+export const trackPurchase = (value: number, currency: string = "MYR") => {
+	ReactPixel.track("Purchase", { value, currency });
 };
 
 export const trackLead = () => {
-    ReactPixel.track('Lead');
+	ReactPixel.track("Lead");
 };
 
-export const trackCustomEvent = (eventName: string, data?: Record<string, any>) => {
-    ReactPixel.trackCustom(eventName, data);
+export const trackCustomEvent = (
+	eventName: string,
+	data?: Record<string, any>,
+) => {
+	ReactPixel.trackCustom(eventName, data);
 };
 
 export default ReactPixel;
